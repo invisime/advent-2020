1759b54be137d3b7a2c1e01a52d82bbf076fad01
Thom Root <thomaslroot@gmail.com>
2020-12-10 12:58:10 -0500

    Day 10 complete

--- 10/solution.rb
+++ 10/solution.rb
@@ -13,13 +13,21 @@ joltage_differences = {1 => 0, 2 => 0, 3 => 1}
 previous_joltage = 0
 adapters.each do |adapter|
 	difference = adapter - previous_joltage
-	raise "invalid adapter" unless joltage_differences.keys.include? difference
+	raise "invalid adapter difference: #{difference} at joltage #{adapter}" unless difference == 1 || difference == 3
 	joltage_differences[difference] += 1
 	previous_joltage = adapter
 end
 
 puts(joltage_differences[1] * joltage_differences[3])
 
-binding.pry
-
 # Part 2
+paths_to_joltage = Hash.new { 0 }
+paths_to_joltage[0] = 1
+[0, *adapters].each do |adapter|
+	1.upto(3) do |jump|
+		desired_joltage = adapter + jump
+		paths_to_joltage[desired_joltage] += paths_to_joltage[adapter] if adapters.include? desired_joltage
+	end
+end
+
+puts paths_to_joltage[adapters.max]
