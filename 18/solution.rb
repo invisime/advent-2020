9b8a0e652f2e5ad1c99b47829c3dc0778144e1ec
Thom Root <thomaslroot@gmail.com>
2020-12-18 12:06:53 -0500

    Day 18 part 1 complete

--- 18/solution.rb
+++ 18/solution.rb
@@ -3,50 +3,39 @@
 require 'pry'
 
 examples = [
-"1 + 2 * 3 + 4 * 5 + 6",
-"1 + (2 * 3) + (4 * (5 + 6))",
-"2 * 3 + (4 * 5)",
-"5 + (8 * 3 + 9 + 3 * 4 * 3)",
-"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
-"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
+	"1 + 2 * 3 + 4 * 5 + 6",
+	"1 + (2 * 3) + (4 * (5 + 6))",
+	"2 * 3 + (4 * 5)",
+	"5 + (8 * 3 + 9 + 3 * 4 * 3)",
+	"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+	"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
 ]
 example_answers = [71, 51, 26, 437, 12240, 13632]
 
-input = examples
-# input = File.readlines(input_file)
+$subexpression_regex = /\(([\d\+\*\s]+)\)/
 
-problems = input.map {|line| line.gsub(/\s/, '').chars }
-
-
-def do_math expression, running_total=0, last_operator='+', length=-1
-	puts ['calculating: ', running_total.to_s, last_operator, *expression, " with length ", length].join
-	return running_total if expression.nil? || expression.empty?
-
-	skip = 0
-	value = case expression.first
-	when /\d/
-		calculate running_total, expression.first.to_i, last_operator
-	when '('
-		do_math expression[1..-1]
-	when ')'
-		running_total
+def do_math problem
+	work = problem.clone
+	while $subexpression_regex.match? work do
+		$subexpression_regex.match work do |match_data|
+			parenthetical, subexpression = match_data.to_a
+			work.sub! parenthetical, do_math(subexpression).to_s
+		end
 	end
-
-	return do_math expression[(skip + 2)..-1], value, expression[skip + 1], length + 2
+	evaluate_simple work.split ' '
 end
 
-def calculate a, b, operator
-	case operator
-	when '+'
-		a + b
-	when '*'
-		a * b
-	else
-		binding.pry
-		raise "unknown operator '#{operator}'"
-	end
+def evaluate_simple expression, running_total=0, operator='+'
+	return running_total if operator.nil?
+	next_total = running_total.send(operator, expression.first.to_i)
+	evaluate_simple expression[2..-1], next_total, expression[1]
 end
 
 # Part 1
+
+problems = File.readlines('input.txt', chomp: true)
+
+answers = problems.map {|problem| do_math problem}
+puts answers.sum
 
 binding.pry
