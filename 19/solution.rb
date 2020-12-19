9122dabf92686425db1a843d41f0a4b850c9d32e
Thom Root <thomaslroot@gmail.com>
2020-12-19 10:06:27 -0500

    Day 19 complete

--- 19/solution.rb
+++ 19/solution.rb
@@ -21,12 +21,12 @@ class Rule
 			@type = :concat
 		end
 
-		@matched_messages = {}
+		@known_messages = {}
 		Rule[@id] = self
 	end
 
 	def matches? message
-		return @matched_messages[message] if @matched_messages.include? message
+		return @known_messages[message] if @known_messages.include? message
 		return false if message.empty?
 		match = case type
 		when :a
@@ -38,11 +38,28 @@ class Rule
 		when :or
 			definition.split('|').any? { |side| Rule.matches_by_concatenation? message, side }
 		end
-		@matched_messages[message] = match
+		@known_messages[message] = match
 	end
 
-	def reset_match_cache
-		@matched_messages = {}
+	def reset_known_messages
+		@known_messages = {}
+	end
+
+	def n_matches? message
+		j = 0
+		repetitions = 0
+		message.length.times do |i|
+			if matches? message[j..i]
+				repetitions += 1
+				j = i + 1
+			end
+		end
+		return nil unless j == message.length
+		repetitions
+	end
+
+	def self.reset_all_known_messages
+		@@by_id.values.each(&:reset_known_messages)
 	end
 
 	def self.matches_by_concatenation? message, rules
@@ -60,7 +77,6 @@ class Rule
 	end
 
 	def self.[]= id, r
-		@@by_id.values.each(&:reset_match_cache) if @@by_id.include? id
 		@@by_id[id] = r
 	end
 
@@ -69,7 +85,24 @@ class Rule
 	end
 
 	def self.zero_matches? message
-		@@by_id["0"].matches? message
+		Rule["0"].matches? message
+	end
+
+	def self.zero_matches_part_2? message
+		# via the new rules 8 and 11 (used only in rule 0) the new rule 0 specifies:
+		#		rule 0 = rule 42 twice or more followed by rule 31 once or more, but
+		#		there must be more 42s than 31s
+
+		1.upto(message.length) do |i|
+			lhs = message[0..-i-1]
+			rhs = message[-i..-1]
+			left_matches = Rule["42"].n_matches? lhs
+			next unless left_matches
+			right_matches = Rule["31"].n_matches? rhs
+			next unless right_matches
+			return true if left_matches > right_matches
+		end
+		false
 	end
 end
 
@@ -84,5 +117,6 @@ rules.each {|r| Rule.new r}
 puts messages.count{|message| Rule.zero_matches? message}
 
 # Part 2
+Rule.reset_all_known_messages
 
-binding.pry
+puts messages.count{|message| Rule.zero_matches_part_2? message}
