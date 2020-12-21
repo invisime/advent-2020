49a23f6b6f41a7e704d9953775114081d885b4fa
Thom Root <thomaslroot@gmail.com>
2020-12-21 11:30:53 -0500

    Day 21 complete

--- 21/solution.rb
+++ 21/solution.rb
@@ -9,30 +9,47 @@ dishes = File.readlines(input_file, chomp: true)
 
 # Part 1
 food_counts = Hash.new { 0 }
-foods_by_alergen = Hash.new
+foods_by_allergen = Hash.new
 
 dishes.each do |dish|
-	foods, alergens = dish[0..-2].split ' (contains '
+	foods, allergens = dish[0..-2].split ' (contains '
 
 	foods = foods.split /\s/
-	alergens = alergens.split ', '
+	allergens = allergens.split ', '
 
 	foods.each { |food| food_counts[food] += 1 }
 
-	alergens.each do |alergen|
-		if foods_by_alergen.include? alergen
-			foods_by_alergen[alergen] &= foods
+	allergens.each do |allergen|
+		if foods_by_allergen.include? allergen
+			foods_by_allergen[allergen] &= foods
 		else
-			foods_by_alergen[alergen] = foods
+			foods_by_allergen[allergen] = foods
 		end
 	end
 end
 
-allergy_free_foods = food_counts.keys - foods_by_alergen.values.flatten
+all_foods = food_counts.keys
+all_allergens = foods_by_allergen.keys
+
+allergy_free_foods = all_foods - foods_by_allergen.values.flatten
 
 puts allergy_free_foods.map{|food| food_counts[food]}.sum
 
-binding.pry
-
-
 # Part 2
+
+unkown_dangerous_foods = all_foods - allergy_free_foods
+dangerous_food_by_allergen = {}
+
+until unkown_dangerous_foods.empty?
+	foods_by_allergen.each do |allergen, foods|
+		candidate_foods = foods - dangerous_food_by_allergen.values
+		next unless candidate_foods.count == 1
+		dangerous_food_by_allergen[allergen] = candidate_foods[0]
+		unkown_dangerous_foods.delete candidate_foods[0]
+	end
+end
+
+
+cannonical_dangerous_food_list = dangerous_food_by_allergen.to_a.sort {|a,b| a[0] <=> b[0]}.map{|kvp| kvp[1]}.join ','
+
+puts cannonical_dangerous_food_list
